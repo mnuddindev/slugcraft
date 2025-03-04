ba10c25e1a6187a1a8c710cf5f508243b04c9a8d
mnuddindev <inadislam@gmail.com>
2025-03-04 17:20:16 +0600

    ✨ Implement in-memory cache for slug management

---
+++ cache.go
@@ -0,0 +1,23 @@
+package slugcraft
+
+// Set adds a slug to the in-memory cache.
+func (c *Cache) Set(slug string) {
+	c.Mu.RLock()
+	defer c.Mu.Unlock()
+	c.Store[slug] = struct{}{}
+}
+
+// Get checks if a slug object exist in the cache
+func (c *Cache) Get(slug string) bool {
+	c.Mu.RLock()
+	defer c.Mu.Unlock()
+	_, exist := c.Store[slug]
+	return exist
+}
+
+// Del removes a slug from the cache.
+func (c *Cache) Del(slug string) {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	delete(c.Store, slug)
+}
